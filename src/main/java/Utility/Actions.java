5de4042e0b41c94f04d640fa6630c980dcf6159a
AKSHATHA S <akshathasujay27@gmail.com>
2025-06-10 22:06:52 +0530

    Maven version has updated

--- src/main/java/Utility/Actions.java
+++ src/main/java/Utility/Actions.java
@@ -5,16 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 public class Actions
 {
     public static WebDriver driver;
     public static void launchChrome()
     {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Run Chrome in headless mode
-        options.addArguments("--no-sandbox"); // Necessary for Jenkins and Docker environments
-        options.addArguments("--disable-dev-shm-usage");
         driver=new ChromeDriver();
         driver.manage().window().maximize();
     }
